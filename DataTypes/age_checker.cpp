9de7b744c4fd6f38695b0468b54b856beaed0fbc
KavishaShah6 <kavisha.sce21@sot.pdpu.ac.in>
2025-07-01 18:17:21 +0530

    Add age_checker.cpp to DataTypes

---
+++ DataTypes/age_checker.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){ 
+    cout << "Enter your age : ";
+    int x;
+    cin >> x;
+    if (x >=18){
+        cout << "You are adult" << endl ;
+
+    }
+    else {
+        cout << "You are not adult" << endl;
+
+    }
+    return 0;
+}
