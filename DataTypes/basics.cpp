8701c3274b7fabe2be7c01f514e415575b9a6f86
KavishaShah6 <kavisha.sce21@sot.pdpu.ac.in>
2025-07-01 17:59:35 +0530

    Add basics of Data Types

---
+++ DataTypes/basics.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 10;
+    long b = 15;
+    long long c = 20000;
+    float d = 3.14f;
+    double e = 3.14159;
+    char f = 'A';
+    bool passed = true;
+    string name = "John";
+
+    cout << "Name: " << name << endl;
+    cout << "Int: " << a << ", Long: " << b << ", Long Long: " << c << endl;
+    cout << "Float: " << d << ", Double: " << e << endl;
+    cout << "Char: " << f << ", Bool: " << passed << endl;
+
+    return 0;
+}
