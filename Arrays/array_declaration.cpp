9ba4c76d21dedbe17964a28f889a6ead3f701ef6
KavishaShah6 <kavisha.sce21@sot.pdpu.ac.in>
2025-07-02 21:49:08 +0530

    Add Array Declaration

---
+++ Arrays/array_declaration.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () { 
+    // Initialization at Declaration
+    int arr1[5] = {1, 2, 3, 4, 5};
+    int arr2[] = {10, 20, 30, 40};
+    int arr3[] = {2, 4, 6, 8};
+
+    // Accessing array elements (index starts from 0)
+    cout << "The fourth element of first array: " << arr1[3] << endl;
+    cout << "The third element of second array: " << arr2[2] << endl;
+    cout << "The third element of third array: " << arr3[2] << endl;
+
+
+    return 0;
+}
