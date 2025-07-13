c5fb53e78fe0c923bd238d316f6c74d6ae8fb02b
Kavisha Shah <kavisha653@gmail.com>
2025-07-13 12:08:33 +0530

    Add for loop example in C++

---
+++ ForLoops/basic_for_loop.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int i;
+    for (i=1;i <=5 ;i++){
+        cout << "Hello this is the : " << i <<"th iteration" << endl ;
+
+    }
+    return 0;
+}
