ebad6d37c500a8e66f16c8b1c2522577589d1bb2
Kavisha Shah <kavisha653@gmail.com>
2025-07-15 13:23:20 +0530

    Create print-numbers-1-to-n.cpp
    Add print number

---
+++ ForLoops/print-numbers-1-to-n.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int n;
+    cout << "Write the number:";
+    cin >> n;
+    for(int i = 1 ; i <=n ; i++){
+        cout << i << endl;
+    }
+    return 0;
+}
