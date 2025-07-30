ced5e71eec1ec4a34afa9d1a296d79c4e2918d10
Kavisha Shah <kavisha653@gmail.com>
2025-07-30 10:32:10 +0530

    Update 2.cpp

--- Patterns/2.cpp
+++ Patterns/2.cpp
@@ -7,7 +7,7 @@ int main() {
         for(int j=1 ;j<=i-1 ;j++){
             cout << "";
         }
-        for(int k =1;k<=2*i-1;k++){
+        for(int n =1;k<=2*i-1;k++){
             cout<<"*";
         }
         cout << endl;
